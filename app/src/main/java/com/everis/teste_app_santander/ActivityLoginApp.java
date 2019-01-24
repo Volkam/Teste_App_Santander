f2a2da703380b7ec2b7d6db37ed7d6e5355bf7c0
cbarboss <caique.barbosasantos@gmail.com>
2019-01-24 14:24:48 -0200

    Inserção do Layout da interface do usuario depois do login e ajustes no Thema padrão(sem a action bar).

--- app/src/main/java/com/everis/teste_app_santander/ActivityLoginApp.java
+++ app/src/main/java/com/everis/teste_app_santander/ActivityLoginApp.java
@@ -3,7 +3,7 @@ package com.everis.teste_app_santander;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityLoginApp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/everis/teste_app_santander/ActivityUserInterface.java
@@ -0,0 +1,13 @@
+package com.everis.teste_app_santander;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityUserInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_interface);
+    }
+}
